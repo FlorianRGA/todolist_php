6f71a15cb450a7b57d542407ce6d0950c7419601
FlorianRGA <FlorianRGA@gmail.com>
2022-10-26 15:54:09 +0200

    finished2

--- controllers/todo_controller.php
+++ controllers/todo_controller.php
@@ -1,5 +1,6 @@
 <?php
 
+$regex0or1 = '/^[01]$/';
 $error = "";
 $incorrectTodo = "";
 $newTodo = "";
@@ -27,32 +28,46 @@ $selectAll = $req->fetchAll();
 
 
 if (isset($_GET["deleteID"])) {
+
     $_GET['deleteID'] = filter_var($_GET['deleteID'], FILTER_SANITIZE_NUMBER_INT);
+
     if (!filter_var($_GET['deleteID'], FILTER_VALIDATE_INT)) {
         header("location: /");
-    };
-    $id = $_GET["deleteID"];
-    $request = "DELETE FROM todo WHERE id= $id";
-    $req = $dbh->prepare($request);
-    $req->execute();
-    header('Location: ./index.php');
+    } else {
+        $id = $_GET["deleteID"];
+        $request = "DELETE FROM todo WHERE id= $id";
+        $req = $dbh->prepare($request);
+        $req->execute();
+        header('Location: ./index.php');
+    }
 } elseif (isset($_GET["changeStateID"])) {
+
     $_GET['changeStateID'] = filter_var($_GET['changeStateID'], FILTER_SANITIZE_NUMBER_INT);
     $_GET['actualState'] = filter_var($_GET['actualState'], FILTER_SANITIZE_NUMBER_INT);
-    if (!filter_var($_GET['changeStateID'], FILTER_VALIDATE_INT) || !filter_var($_GET['actualState'], FILTER_VALIDATE_INT)) {
-        header("location: /");
-    };
-    $id = $_GET["changeStateID"];
-    $state = $_GET["actualState"];
-    if ($state === "0") {
-        $state = 1;
+
+    if (filter_var($_GET['changeStateID'], FILTER_VALIDATE_INT)) {
+        if (preg_match($regex0or1, $_GET['actualState'])) {
+            $id = $_GET["changeStateID"];
+            $state = $_GET["actualState"];
+            if ($state === "0") {
+                $state = 1;
+            } else {
+                $state = 0;
+            }
+            $request = "UPDATE todo
+            SET done = $state
+            WHERE id=$id";
+            $req = $dbh->prepare($request);
+            $req->execute();
+            header('Location: ./index.php');
+        } else {
+            // var_dump('c pa ok');
+            // die();
+            header("location: /");
+        }
     } else {
-        $state = 0;
+        // var_dump('c pa ok');
+        // die();
+        header("location: /");
     }
-    $request = "UPDATE todo
-    SET done = $state
-    WHERE id=$id";
-    $req = $dbh->prepare($request);
-    $req->execute();
-    header('Location: ./index.php');
 }
